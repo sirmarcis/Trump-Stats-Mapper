86a2d1b84fc31536b8b7c62d5ecd41b68fa207f1
fiaccs <fiaccs@rpi.edu>
2016-04-24 16:31:54 -0400

    server

--- apps/control/webserver.py
+++ apps/control/webserver.py
@@ -1,5 +1,8 @@
 import sys
 import os
+from flask import Flask
+app = Flask(__name__)
+
 
 def get_backend_filepath():
 	filepath = os.path.dirname(os.path.realpath(__file__))
@@ -9,9 +12,19 @@ def get_backend_filepath():
 sys.path.append(get_backend_filepath())
 import database
 
+@app.route('/')
+def index():
+  return render_template('index_test.html')
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
+
+
 def main():
 	print get_backend_filepath()
 
 if __name__ == "__main__":
 	main()
+	app.run(debug=True)
 
